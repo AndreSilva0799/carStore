fbdc706e37fa3ad53edba6d0a033e4978e018b76
Andre Victor <andrevictor2102@hotmail.com>
2024-03-27 21:17:48 -0300

    aula 02 do projeto

--- src/main/java/br/com/carstore/sevlet/CreateCarServlet.java
+++ src/main/java/br/com/carstore/sevlet/CreateCarServlet.java
@@ -1,11 +1,15 @@
 package br.com.carstore.sevlet;
 
+import br.com.carstore.dao.CarDAO;
+import br.com.carstore.model.Car;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/create-car")
 public class CreateCarServlet extends HttpServlet {
@@ -13,6 +17,17 @@ public class CreateCarServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
 
         String carName = request.getParameter("carName");
+
+        CarDAO carDao = new CarDAO();
+
+        Car car = new Car(carName);
+
+
+        car.setName(carName);
+
+        carDao.createCar(car);
+
+
         System.out.println("o nome do carro é: " + carName);
         request.getRequestDispatcher("index.html").forward(request,resp);
 
